13b5acb4e100376256e49a3532187ffe39a86344
Rubens Luiz de Alcantara de Souza <rm86609@estudante.fieb.edu.br>
2023-04-27 09:49:00 -0300

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+        //laço enquanto - while
+        int contador =0;
+        while(contador < 10) {
+            System.out.println("contador = "+contador);
+            // incremento
+            contador = contador +1;
+
+        }
+        //laço para faça - for
+        for(int x = 0; x<10;x++) {
+            System.out.println("contador ="+ x);
+        }
+        // faça enquanto - do while
+        int cont=1;
+        do{
+            //processo
+            System.out.println(cont);
+            cont++; // incremento
+        }while(cont <=10);
+    }
+}
